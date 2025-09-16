8f9adca9bf78e93c1d0a4104c3b83cf4cfad95a5
kureung <terry9611@naver.com>
2025-09-17 00:55:57 +0900

    feat: 타임스탬프 ID 생성 기능

--- src/main/java/TimestampBasedIdGenerator.java
+++ src/main/java/TimestampBasedIdGenerator.java
@@ -4,8 +4,12 @@ import java.util.Objects;
 public final class TimestampBasedIdGenerator {
     private final Instant basedEpoch;
 
-    public TimestampBasedIdGenerator(Instant basedEpoch) {
+    public TimestampBasedIdGenerator(final Instant basedEpoch) {
         Objects.requireNonNull(Objects.requireNonNull(basedEpoch));
         this.basedEpoch = basedEpoch;
+    }
+
+    public Long generate(final Instant baseTime) {
+        return baseTime.toEpochMilli() - basedEpoch.toEpochMilli();
     }
 }
--- src/test/java/TimestampBasedIdGeneratorTest.java
+++ src/test/java/TimestampBasedIdGeneratorTest.java
@@ -1,5 +1,7 @@
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.Instant;
 import org.junit.jupiter.api.Test;
 
 class TimestampBasedIdGeneratorTest {
@@ -8,5 +10,25 @@ class TimestampBasedIdGeneratorTest {
     void null_입력시_예외가_발생한다() {
         assertThatThrownBy(() -> new TimestampBasedIdGenerator(null))
                 .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void 기준시간에_ID값을_생성할_경우_0이_생성된다() {
+        final var basedEpoch = Instant.parse("2025-01-01T00:00:00Z");
+        final var sut = new TimestampBasedIdGenerator(basedEpoch);
+
+        final var id = sut.generate(basedEpoch);
+        assertThat(id).isZero();
+    }
+
+    @Test
+    void 기준시간으로부터_1s_후_ID는_1000ms다() {
+        final var basedEpoch = Instant.parse("2025-01-01T00:00:00Z");
+        final var sut = new TimestampBasedIdGenerator(basedEpoch);
+
+        final var basedEpochAfterOneSecond = basedEpoch.plusSeconds(1);
+        final var id = sut.generate(basedEpochAfterOneSecond);
+
+        assertThat(id).isEqualTo(1000L);
     }
 }
