61f53a8910b43803d668850972e24d0afac0e787
kureung <terry9611@users.noreply.github.com>
2025-10-09 19:39:48 +0900

    feat: InstagramId한테 아이디 생성 로직 위임

--- src/main/java/InstagramId.java
+++ src/main/java/InstagramId.java
@@ -1,27 +1,59 @@
-import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
-public class InstagramId {
-    private final long value;
+public final class InstagramId {
+    private final long timestamp;
+    private final long shardId;
+    private final long sequence;
+    private final int shardIdBits;
+    private final int sequenceBits;
+    private final AtomicReference<Long> idValue = new AtomicReference<>();
 
-    private InstagramId(long value) {
-        this.value = value;
+    private InstagramId(
+            final long timestamp,
+            final long shardId,
+            final long sequence,
+            final int shardIdBits,
+            final int sequenceBits
+    ) {
+        this.timestamp = timestamp;
+        this.shardId = shardId;
+        this.sequence = sequence;
+        this.shardIdBits = shardIdBits;
+        this.sequenceBits = sequenceBits;
     }
 
-    public static InstagramId from(long value) {
-        return new InstagramId(value);
+    public static InstagramId of(
+            final long timestamp,
+            final long shardId,
+            final long sequence,
+            final int shardIdBits,
+            final int sequenceBits
+    ) {
+        return new InstagramId(timestamp, shardId, sequence, shardIdBits, sequenceBits);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
+    public long idValue() {
+        Long result = idValue.get();
+        if (result == null) {
+            result = combine(timestamp, shardId, sequence, shardIdBits, sequenceBits);
+            idValue.compareAndSet(null, result);
+            result = idValue.get();
         }
-        InstagramId that = (InstagramId) o;
-        return value == that.value;
+        return result;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(value);
+    private static long combine(
+            final long timestamp,
+            final long shardId,
+            final long sequence,
+            final int shardIdBits,
+            final int sequenceBits
+    ) {
+        final int timestampShift = shardIdBits + sequenceBits;
+
+        var id = timestamp << timestampShift;
+        id |= shardId << sequenceBits;
+        id |= sequence;
+        return id;
     }
 }
