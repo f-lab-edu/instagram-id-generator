be316cc15ee5c6967bb380839d7b2a190a78e79f
kureung <terry9611@users.noreply.github.com>
2025-10-09 19:40:26 +0900

    test: InstagramId에 원시 id 값 생성 테스트

--- src/test/java/InstagramIdTest.java
+++ src/test/java/InstagramIdTest.java
@@ -14,4 +14,17 @@ class InstagramIdTest {
 
         assertThat(id.idValue()).isEqualTo(22468047667201025L);
     }
+
+    @Test
+    @DisplayName("timestamp, shardId, sequence를 비트 연산하여 raw ID를 반환한다")
+    void generator_id_as_raw_id() {
+        final var timestamp = 2678400000L;
+        final var shardId = 1L;
+        final var sequence = 1L;
+
+        final var result = InstagramId.makeRawId(timestamp, shardId, sequence, 13, 10);
+
+        assertThat(result).isEqualTo(22468047667201025L);
+    }
+
 }
