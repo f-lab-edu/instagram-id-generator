8e3f81478167ac69c282063ebbe335d7db167ace
kureung <terry9611@users.noreply.github.com>
2025-10-22 16:24:17 +0900

    feat: id 구성 요소의 비트 관리 기능 추가

---
+++ src/main/java/IdComponent.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public enum IdComponent {
+    TIMESTAMP(41),
+    SHARD(13),
+    SEQUENCE(10);
+
+    static {
+        long totalBits = Arrays.stream(IdComponent.values())
+                .mapToLong(IdComponent::bitLength)
+                .sum();
+        if (totalBits > Long.SIZE) {
+            throw new IllegalStateException("ID 구성의 비트 합이 64이어야 한다");
+        }
+    }
+
+    private final long bitLength;
+
+    IdComponent(final long bitLength) {
+        this.bitLength = bitLength;
+    }
+
+    public long bitLength() {
+        return bitLength;
+    }
+
+    public long maxValue() {
+        return (1L << bitLength) - 1;
+    }
+}
